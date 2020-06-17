26f68023331f8ff48398e7e23a45e86588358cd1
Shalini DC <shalini.1325@gmail.com>
2020-06-17 12:21:03 -0700

    adding front end build for static display from the backend

---
+++ build/precache-manifest.ea432d50d27597f3026a4f72e8ea851d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae6755ef5b08493c47661a9a4a048e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0495defa0ac3c984ea9",
+    "url": "/static/css/main.cc8249d7.chunk.css"
+  },
+  {
+    "revision": "701a65f4b8ee522b950d",
+    "url": "/static/js/2.f79a66cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f79a66cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0495defa0ac3c984ea9",
+    "url": "/static/js/main.d4569c52.chunk.js"
+  },
+  {
+    "revision": "4d506a819328d5b617fc",
+    "url": "/static/js/runtime-main.f65b7067.js"
+  }
+]);
